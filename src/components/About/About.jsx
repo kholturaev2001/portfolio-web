e28c1907d6f806e5774f22c5c448d69731e99fdd
mirakov <holturaevm@gmail.com>
2022-10-30 23:58:31 +0500

    first commit of my portfolio web-page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./about.css";
+import ME from "../../assetsOne/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me_image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>3+ Years Working</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            doloribus in quibusdam facere? Consequuntur, officia molestiae,
+            adipisci veritatis in tenetur voluptate fugit nemo dolor debitis
+            laborum provident saepe et modi?
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CTA from "./CTA";
+import "./header.css";
+import ME from "../../assetsOne/me.png";
+import HeaderSocials from "./HeaderSocials";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello, I'm</h5>
+        <h1>Muhammadrasul Kholturaev</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
