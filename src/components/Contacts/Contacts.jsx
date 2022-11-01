8a4be1aa481519177f480af62f0f57ae9aabf958
mirakov <holturaevm@gmail.com>
2022-11-01 09:01:41 +0500

    contacts in process

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import './contacts.css'
+import React from "react";
+import "./contacts.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contacts = () => {
   return (
-    <section id='contact'>Contacts</section>
-  )
-}
+    <section id="contact">
+      <h5>Get In Toush</h5>
+      <h2>Contact Me</h2>
 
-export default Contacts
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail className='contact_option-icon' />
+            <h4>Email</h4>
+            <h5>holturaevm@gmail.com</h5>
+            <a href="mailto:holturaevm@gmail.com" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact_option">
+            <RiMessengerLine className='contact_option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Muhammadrasul</h5>
+            <a href="https://m.me/{paste profile link}" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp className='contact_option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+992904018077</h5>
+            <a href="https://api.whatsapp.com/send?phone+992904018077" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name='email' placeholder="Your Email" required />
+          <textarea name="message" rows="7" required></textarea>
+          <button type="submit" className="btn btn-primary" >Send Message</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contacts;
